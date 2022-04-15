b97b916b33e6d9ccd410657b103b54a4cd494b06
Al Amin <alamin.aiub10@gmail.com>
2022-04-15 16:40:12 +0600

    twin tower added

--- main.cpp
+++ main.cpp
@@ -83,123 +83,134 @@ void river(){
     glEnd();
 }
 
-void cloud_struct()
-{
-        glColor3f(1.000, 1.000, 1.000);
-        glScalef(0.6,1,1);
-        glBegin(GL_POLYGON);
-        for(int i=0;i<200;i++)
-        {
-            float pi=3.1416;
-            float A=(i*2*pi)/200;
-            float r=0.075;
-            float x = r * cos(A);
-            float y = r * sin(A);
-            glVertex2f(x,y );
-        }
-        glEnd();
+void tower(){
+    glBegin(GL_QUADS);
+    glColor3f(1.0, 1.0, 1.0);
+    glVertex2f(-0.28, -0.03);
+    glVertex2f(-0.20, -0.03);
+    glVertex2f(-0.20, 0.05);
+    glVertex2f(-0.28, 0.05);
+    glEnd();
+
 }
 
+void twin_tower(){
+    //Tower 1 of twin tower
+    glBegin(GL_QUADS);
+    glColor3f(0.647, 0.165, 0.165);
+    glVertex2f(-0.30, -0.05);
+    glVertex2f(-0.08, -0.05);
+    glVertex2f(-0.08, 0.60);
+    glVertex2f(-0.30, 0.60);
+    glEnd();
 
-void cloud_left(){
-    //Cloud on the left
-    glLoadIdentity(); //Reset the drawing perspective
-    glMatrixMode(GL_MODELVIEW);
+    glBegin(GL_QUADS);
+    glColor3f(0.502, 0.000, 0.000);
+    glVertex2f(-0.31, 0.60);
+    glVertex2f(-0.07, 0.60);
+    glVertex2f(-0.07, 0.62);
+    glVertex2f(-0.31, 0.62);
+    glEnd();
+    //Roof top of tower 1
+
+
+    //1st floor window of tower 1
+    tower();
+
     glPushMatrix();
-    glTranslatef(0.0f, 0.0f, 0.0f);
-    glTranslatef(-0.78,0.82,0);
-    cloud_struct();
+    glTranslatef(0.0,0.10,0);
+    tower();
+    glTranslatef(0.0,0.10,0);
+    tower();
+    glTranslatef(0.0,0.10,0);
+    tower();
+    glTranslatef(0.0,0.10,0);
+    tower();
+    glTranslatef(0.0,0.10,0);
+    tower();
     glPopMatrix();
 
     glPushMatrix();
-    glTranslatef(0.0f, 0.0f, 0.0f);
-    glTranslatef(-0.74,0.87,0);
-    cloud_struct();
+    glTranslatef(0.10,0.0,0);
+    tower();
+    glTranslatef(0.0,0.1,0);
+    tower();
+    glTranslatef(0.0,0.10,0);
+    tower();
+    glTranslatef(0.0,0.10,0);
+    tower();
+    glTranslatef(0.0,0.10,0);
+    tower();
+    glTranslatef(0.0,0.10,0);
+    tower();
+    glPopMatrix();
+
+
+    //Tower 2 of twin tower
+    glBegin(GL_QUADS);
+    glColor3f(0.647, 0.165, 0.165);
+    glVertex2f(0.08, -0.05);
+    glVertex2f(0.30, -0.05);
+    glVertex2f(0.30, 0.60);
+    glVertex2f(0.08, 0.60);
+    glEnd();
+
+    //Roof top of tower 2
+    glBegin(GL_QUADS);
+    glColor3f(0.502, 0.000, 0.000);
+    glVertex2f(0.31, 0.60);
+    glVertex2f(0.07, 0.60);
+    glVertex2f(0.07, 0.62);
+    glVertex2f(0.31, 0.62);
+    glEnd();
+
+
+    glPushMatrix();
+    glTranslatef(0.38,0.0,0);
+    tower();
+    glTranslatef(0.0,0.1,0);
+    tower();
+    glTranslatef(0.0,0.1,0);
+    tower();
+    glTranslatef(0.0,0.1,0);
+    tower();
+    glTranslatef(0.0,0.1,0);
+    tower();
+    glTranslatef(0.0,0.1,0);
+    tower();
     glPopMatrix();
 
     glPushMatrix();
-    glTranslatef(0.0f, 0.0f, 0.0f);
-    glTranslatef(-0.70,0.91,0);
-    cloud_struct();
+    glTranslatef(0.48,0.0,0);
+    tower();
+    glTranslatef(0.0,0.1,0);
+    tower();
+    glTranslatef(0.0,0.1,0);
+    tower();
+    glTranslatef(0.0,0.1,0);
+    tower();
+    glTranslatef(0.0,0.1,0);
+    tower();
+    glTranslatef(0.0,0.1,0);
+    tower();
     glPopMatrix();
 
-    glPushMatrix();
-    glTranslatef(0.0f, 0.0f, 0.0f);
-    glTranslatef(-0.65,0.88,0);
-    cloud_struct();
-    glPopMatrix();
+    //Joining of twin tower
+    glBegin(GL_QUADS);
+    glColor3f(0.647, 0.165, 0.165);
+    glVertex2f(-0.08, 0.15);
+    glVertex2f(0.08, 0.15);
+    glVertex2f(0.08, 0.27);
+    glVertex2f(-0.08, 0.27);
 
-    glPushMatrix();
-    glTranslatef(0.0f, 0.0f, 0.0f);
-    glTranslatef(-0.61,0.80,0);
-    cloud_struct();
-    glPopMatrix();
+    glBegin(GL_QUADS);
+    glColor3f(1.0, 1.0, 1.0);
+    glVertex2f(-0.08, 0.17);
+    glVertex2f(0.08, 0.17);
+    glVertex2f(0.08, 0.25);
+    glVertex2f(-0.08, 0.25);
 
-    glPushMatrix();
-    glTranslatef(0.0f, 0.0f, 0.0f);
-    glTranslatef(-0.68,0.77,0);
-    cloud_struct();
-    glPopMatrix();
-
-    glPushMatrix();
-    glTranslatef(0.0f, 0.0f, 0.0f);
-    glTranslatef(-0.75,0.78,0);
-    cloud_struct();
-    glPopMatrix();
-}
-
-void cloud_right(){
-    //Cloud on the right
-    glLoadIdentity(); //Reset the drawing perspective
-	glMatrixMode(GL_MODELVIEW);
-
-    glPushMatrix();
-    glTranslatef(0.0f, 0.0f, 0.0f);
-    glTranslatef(0.78,0.70,0);
-    cloud_struct();
-    glPopMatrix();
-
-    glPushMatrix();
-    glTranslatef(0.0f, 0.0f, 0.0f);
-    glTranslatef(0.74,0.75,0);
-    cloud_struct();
-    glPopMatrix();
-
-    glPushMatrix();
-    glTranslatef(0.0f, 0.0f, 0.0f);
-    glTranslatef(0.70,0.79,0);
-    cloud_struct();
-    glPopMatrix();
-
-    glPushMatrix();
-    glTranslatef(0.0f, 0.0f, 0.0f);
-    glTranslatef(0.65,0.76,0);
-    cloud_struct();
-    glPopMatrix();
-
-    glPushMatrix();
-    glTranslatef(0.0f, 0.0f, 0.0f);
-    glTranslatef(0.61,0.76,0);
-    cloud_struct();
-    glPopMatrix();
-
-    glPushMatrix();
-    glTranslatef(0.0f, 0.0f, 0.0f);
-    glTranslatef(0.68,0.65,0);
-    cloud_struct();
-    glPopMatrix();
-
-    glPushMatrix();
-    glTranslatef(0.0f, 0.0f, 0.0f);
-    glTranslatef(0.63,0.66,0);
-    cloud_struct();
-    glPopMatrix();
-
-    glPushMatrix();
-    glTranslatef(0.0f, 0.0f, 0.0f);
-    glTranslatef(0.75,0.66,0);
-    cloud_struct();
-    glPopMatrix();
+    glEnd();
 }
 
 
@@ -220,8 +231,7 @@ void myDisplay(void){
     road();
     field_2();
     river();
-    cloud_left();
-    cloud_right();
+    twin_tower();
 
     glFlush();
 }
@@ -243,6 +253,5 @@ int main(int argc, char** argv){
     myInit ();
     glutIdleFunc(Idle);
     glutMainLoop();
-    
-}
 
+}
