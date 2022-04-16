f9261e85a3bcd9ef0ccc4d7fe2ec441325d27f8a
atiquepro <70340707+atiquepro@users.noreply.github.com>
2022-04-16 16:17:20 +0600

    right side houses

--- main.cpp
+++ main.cpp
@@ -83,6 +83,115 @@ void river(){
     glEnd();
 }
 
+void house_1(){
+    //House 1
+    glBegin(GL_QUADS);
+    glColor3f(0.400, 0.804, 0.667);
+    glVertex2f(0.45, -0.03);
+    glVertex2f(0.72, -0.03);
+    glVertex2f(0.72, 0.17);
+    glVertex2f(0.45, 0.17);
+
+    //Door of House 1
+    glBegin(GL_QUADS);
+    glColor3f(1.000, 0.894, 0.882);
+    glVertex2f(0.56, -0.03);
+    glVertex2f(0.60, -0.03);
+    glVertex2f(0.60, 0.12);
+    glVertex2f(0.56, 0.12);
+
+    //Window of House 1
+    glBegin(GL_QUADS);
+    glColor3f(1.000, 1.000, 1.000);
+    glVertex2f(0.48, 0.03);
+    glVertex2f(0.52, 0.03);
+    glVertex2f(0.52, 0.10);
+    glVertex2f(0.48, 0.10);
+
+    glBegin(GL_QUADS);
+    glColor3f(1.000, 1.000, 1.000);
+    glVertex2f(0.64, 0.03);
+    glVertex2f(0.68, 0.03);
+    glVertex2f(0.68, 0.10);
+    glVertex2f(0.64, 0.10);
+
+    //Basement of house 1
+    glBegin(GL_QUADS);
+    glColor3f(0.647, 0.165, 0.165);
+    glVertex2f(0.44, -0.05);
+    glVertex2f(0.73, -0.05);
+    glVertex2f(0.73, -0.03);
+    glVertex2f(0.44, -0.03);
+
+    //Roof of house 1
+    glBegin(GL_QUADS);
+    glColor3f(1.000, 0.855, 0.725);
+    glVertex2f(0.44, 0.17);
+    glVertex2f(0.73, 0.17);
+    glVertex2f(0.73, 0.19);
+    glVertex2f(0.44, 0.19);
+    glEnd();
+
+    //Roof of house 1
+    glBegin(GL_POLYGON);
+    glColor3f(1.000, 0.937, 0.835);
+    glVertex2f(0.44, 0.19);
+    glVertex2f(0.73, 0.19);
+    glVertex2f(0.59, 0.27);
+    glEnd();
+}
+
+void house_2(){
+    //House 2
+    glBegin(GL_QUADS);
+    glColor3f(0.804, 0.361, 0.361);
+    glVertex2f(0.78, -0.03);
+    glVertex2f(0.95, -0.03);
+    glVertex2f(0.95, 0.17);
+    glVertex2f(0.78, 0.17);
+
+    //Door of House 2
+    glBegin(GL_QUADS);
+    glColor3f(1.000, 1.000, 1.000);
+    glVertex2f(0.87, -0.03);
+    glVertex2f(0.92, -0.03);
+    glVertex2f(0.92, 0.12);
+    glVertex2f(0.87, 0.12);
+
+    //Window of House 2
+    glBegin(GL_QUADS);
+    glColor3f(1.000, 1.000, 1.000);
+    glVertex2f(0.80, 0.03);
+    glVertex2f(0.84, 0.03);
+    glVertex2f(0.84, 0.10);
+    glVertex2f(0.80, 0.10);
+
+    //Basement of house 2
+    glBegin(GL_QUADS);
+    glColor3f(0.647, 0.165, 0.165);
+    glVertex2f(0.77, -0.05);
+    glVertex2f(0.96, -0.05);
+    glVertex2f(0.96, -0.03);
+    glVertex2f(0.77, -0.03);
+
+    //Roof of house 2
+    glBegin(GL_QUADS);
+    glColor3f(1.000, 0.855, 0.725);
+    glVertex2f(0.77, 0.17);
+    glVertex2f(0.96, 0.17);
+    glVertex2f(0.96, 0.19);
+    glVertex2f(0.77, 0.19);
+    glEnd();
+
+    //Roof of house 2
+    glBegin(GL_POLYGON);
+    glColor3f(1.000, 0.937, 0.835);
+    glVertex2f(0.77, 0.19);
+    glVertex2f(0.96, 0.19);
+    glVertex2f(0.86, 0.25);
+    glEnd();
+}
+
 
 void myInit (void){
     glClearColor(1.0, 1.0, 1.0, 0.0);
@@ -101,6 +210,8 @@ void myDisplay(void){
     road();
     field_2();
     river();
+    house_1();
+    house_2();
 
     glFlush();
 }
@@ -122,4 +233,5 @@ int main(int argc, char** argv){
     myInit ();
     glutIdleFunc(Idle);
     glutMainLoop();
+
 }
